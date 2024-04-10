69a8cf5408a5c8fe54cfe5db4ff7216139468fa2
Tallone <tallone.shi@outlook.com>
2024-04-10 16:09:38 +0800

    🔥  Remove unused import

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,21 +1,10 @@
 use std::{net::SocketAddr, time::Duration};
 
 use anyhow::Ok;
-use axum::{
-    http::{HeaderName, HeaderValue},
-    Router,
-};
+use axum::Router;
 use dotenvy::dotenv;
 use log::info;
 use tokio::sync::broadcast;
-use tower::ServiceBuilder;
-use tower::util::MapResponseLayer;
-use tower_http::{
-    cors::{Any, CorsLayer},
-    propagate_header::PropagateHeaderLayer,
-    set_header::SetRequestHeaderLayer,
-};
-use uuid::Uuid;
 
 use crate::internal::{app_state::AppState, conf::ApplicationConf, db::DB, logger};
 
@@ -26,32 +15,32 @@ mod middleware;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    dotenv().expect(".env file not found");
+  dotenv().expect(".env file not found");
 
-    // Initialize logger
-    logger::init();
+  // Initialize logger
+  logger::init();
 
-    // Parse configuration from environment
-    let conf = ApplicationConf::from_env();
+  // Parse configuration from environment
+  let conf = ApplicationConf::from_env();
 
-    // Connect to database
-    let db = DB::new(&conf).await?;
+  // Connect to database
+  let db = DB::new(&conf).await?;
 
-    // The shutdown channel
-    let (_, rx) = broadcast::channel::<()>(8);
+  // The shutdown channel
+  let (_, rx) = broadcast::channel::<()>(8);
 
-    // Start task system
-    task::start_tick(Duration::from_millis(100), rx);
+  // Start task system
+  task::start_tick(Duration::from_millis(100), rx);
 
-    let state = AppState { db };
-    let app = Router::new()
-        .nest("/api", biz::router())
-        .with_state(state)
-        .fallback(middleware::response_wrapper::handle_404);
-    let app = middleware::init(app);
+  let state = AppState { db };
+  let app = Router::new()
+    .nest("/api", biz::router())
+    .with_state(state)
+    .fallback(middleware::response_wrapper::handle_404);
+  let app = middleware::init(app);
 
-    let listener = tokio::net::TcpListener::bind(&conf.server.addr).await?;
-    info!("listening on {}", listener.local_addr().unwrap());
-    axum::serve(listener, app.into_make_service_with_connect_info::<SocketAddr>()).await?;
-    Ok(())
+  let listener = tokio::net::TcpListener::bind(&conf.server.addr).await?;
+  info!("listening on {}", listener.local_addr().unwrap());
+  axum::serve(listener, app.into_make_service_with_connect_info::<SocketAddr>()).await?;
+  Ok(())
 }
